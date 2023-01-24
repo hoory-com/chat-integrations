89f75ad671af8632dba77564e257f8c51756b32b
ArtyomFahradyan <artyom.fahradyan9554@gmail.com>
2023-01-24 12:29:51 +0400

    Move ucraft forms

---
+++ src/integrations/ucraft/LikedWebsite/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  SSO_URL,
+  AUTH_TOKEN_KEY,
+} from "../../../../../../../helpers/ucraftHelpers/constants";
+import FormButton from "../../../../../FormButton";
+import { useRasaMessageContext } from "../../../context";
+import { useUpdateMessages } from "../../utils";
+import { ChatPagePostMessage } from "../../../../../../../constants";
+import { useFormsContext } from "../../FormsContext";
+import { ButtonsWrapper } from "../../styles";
+
+function LikedWebsite() {
+  const { t } = useTranslation("ui");
+  const {
+    field: { field_metadata, value },
+  } = useFormsContext();
+  const [completed, setCompleted] = useState(Boolean(value));
+  const { updateMessages } = useUpdateMessages();
+  const { sendMessageHandler, message, color, toggleAiTemplateIframe } =
+    useRasaMessageContext();
+
+  const listener = useCallback(
+    ({
+      data: { type, data },
+    }: {
+      data: {
+        type: ChatPagePostMessage;
+        data: { [key: string]: string };
+      };
+    }) => {
+      if (type === ChatPagePostMessage.SET_AI_TEMPLATE_RANDOM_ID) {
+        sendMessageHandler &&
+          sendMessageHandler({
+            message: t("rasaForm.notForNowTextBtnText"),
+            metadata: {
+              templateId: data.selectedAiId,
+              updateId: message.id,
+            },
+            updateActionsBody: updateMessages(data.selectedAiId),
+          });
+        setCompleted(true);
+      }
+    },
+    [message]
+  );
+
+  useEffect(() => {
+    window.addEventListener("message", listener, false);
+    return () => {
+      window.removeEventListener("message", listener);
+    };
+  }, [listener]);
+
+  const handleConfirm = () => {
+    const text = t("rasaForm.yesIHaveBtnText");
+    sendMessageHandler &&
+      sendMessageHandler({
+        message: text,
+        metadata: {
+          updateId: message.id,
+        },
+        updateActionsBody: updateMessages(text),
+      });
+    setCompleted(true);
+  };
+
+  const handleNotNow = () => {
+    toggleAiTemplateIframe &&
+      toggleAiTemplateIframe({
+        data: {
+          address: SSO_URL,
+          token: localStorage.getItem(AUTH_TOKEN_KEY),
+          projectUrl: field_metadata?.projectUrl || "",
+          industry: field_metadata?.industry || "",
+        },
+      });
+  };
+
+  return (
+    <ButtonsWrapper>
+      <FormButton
+        onClick={handleConfirm}
+        disabled={completed}
+        color={color}
+        block
+      >
+        {t("rasaForm.yesIHaveBtnTextEmoji")}
+      </FormButton>
+      <FormButton
+        onClick={handleNotNow}
+        disabled={completed}
+        color={color}
+        block
+      >
+        {t("rasaForm.notForNowTextBtnTextEmoji")}
+      </FormButton>
+    </ButtonsWrapper>
+  );
+}
+
+export default LikedWebsite;
---
+++ src/integrations/ucraft/LogoUploader/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useRasaMessageContext } from "../../../context";
+import Base64Uploader from "../../../../../Base64Uploader";
+import {
+  UPLOADED_LOGO_BASE64_STORAGE_KEY,
+  RASA_NO_LOGO,
+  RASA_UPLOAD_COMPLETED,
+} from "../../../../../../../helpers/ucraftHelpers/constants";
+import { useUpdateMessages } from "../../utils";
+import { useFormsContext } from "../../FormsContext";
+import { StyledButton } from "../../../styles";
+
+function LogoUploader() {
+  const { t } = useTranslation("ui");
+  const {
+    field: { value },
+  } = useFormsContext();
+  const { sendMessageHandler, color, message } = useRasaMessageContext();
+  const { updateMessages } = useUpdateMessages();
+  const logo = localStorage.getItem(UPLOADED_LOGO_BASE64_STORAGE_KEY);
+  const [uploadedLogo, setUploadedLogo] = useState(logo || "");
+  const [isSaved, setIsSaved] = useState(
+    Boolean(value && value === RASA_UPLOAD_COMPLETED)
+  );
+  const [isSkipped, setIsSkipped] = useState(
+    Boolean(value && value === RASA_NO_LOGO)
+  );
+
+  function saveLogo() {
+    localStorage.setItem(UPLOADED_LOGO_BASE64_STORAGE_KEY, uploadedLogo);
+    sendMessageHandler &&
+      sendMessageHandler({
+        message: RASA_UPLOAD_COMPLETED,
+        metadata: {
+          updateId: message.id,
+        },
+        updateActionsBody: updateMessages(RASA_UPLOAD_COMPLETED),
+      });
+    setIsSaved(true);
+  }
+
+  function skipStep() {
+    setIsSkipped(true);
+    sendMessageHandler &&
+      sendMessageHandler({
+        message: RASA_NO_LOGO,
+        metadata: {
+          updateId: message.id,
+        },
+        updateActionsBody: updateMessages(RASA_NO_LOGO),
+      });
+  }
+
+  return (
+    <>
+      <Base64Uploader
+        sizeLimit={5}
+        setUploadedLogo={setUploadedLogo}
+        uploadedLogo={uploadedLogo}
+        isSaved={isSaved}
+        isSkipped={isSkipped}
+      />
+      {!isSaved && !isSkipped && (
+        <StyledButton
+          color={color}
+          isPrimary={Boolean(uploadedLogo)}
+          onClick={uploadedLogo ? saveLogo : skipStep}
+          htmlType="submit"
+        >
+          {t(`rasaForm.${uploadedLogo ? "confirm" : "skipBtn"}`)}
+        </StyledButton>
+      )}
+    </>
+  );
+}
+
+export default LogoUploader;
